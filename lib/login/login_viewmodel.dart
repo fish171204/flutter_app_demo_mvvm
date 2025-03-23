fd44c998865a3de7f88a8b9ecae42fc18a36425e
Nguyễn Đăng Khoa <145481912+fish171204@users.noreply.github.com>
2025-03-23 23:02:59 +0700

    feat(login): add validation streams for email and password

---
+++ lib/login/login_viewmodel.dart
@@ -0,0 +1,53 @@
+// StreamTransformer giúp xử lý dữ liệu đầu vào trước khi nó đến được các StreamBuilder hoặc các phần khác của ứng dụng.
+// Khi người dùng nhập email,pass... StreamTransformer sẽ kiểm tra tính hợp lệ của email,pass... đó.
+import 'dart:async';
+import 'package:flutter/foundation.dart';
+import 'package:flutter_app_demo_mvvm/helper/validation.dart';
+import 'package:rxdart/rxdart.dart';
+
+class LoginViewmodel {
+  final _emailSubject = BehaviorSubject<String>();
+  final _passSubject = BehaviorSubject<String>();
+  final _btnSubject = BehaviorSubject<bool>();
+
+  var emailValidation = StreamTransformer<String, String?>.fromHandlers(
+      handleData: (email, sink) {
+    final error = Validation.validateEmail(email);
+    sink.add(error); // Phát null nếu email hợp lệ
+  });
+
+  var passValidation =
+      StreamTransformer<String, String?>.fromHandlers(handleData: (pass, sink) {
+    final error = Validation.validatePass(pass);
+    sink.add(error); // Phát null nếu pass hợp lệ
+  });
+
+  // .transform(emailValidation): Áp dụng StreamTransformer để kiểm tra và xử lý dữ liệu trước khi gửi đi.
+  Stream<String?> get emailStream =>
+      _emailSubject.stream.transform(emailValidation);
+  Sink<String> get emailSink => _emailSubject.sink;
+
+  // .transform(passValidation): Áp dụng StreamTransformer để kiểm tra và xử lý dữ liệu trước khi gửi đi.
+  Stream<String?> get passStream =>
+      _passSubject.stream.transform(passValidation);
+  Sink<String> get passSink => _passSubject.sink;
+
+  Stream<bool> get btnStream => _btnSubject.stream;
+  Sink<bool> get btnSink => _btnSubject.sink;
+
+  LoginViewmodel() {
+    // Kết hợp 2 stream
+    Rx.combineLatest2(_emailSubject, _passSubject, (String email, String pass) {
+      return Validation.validateEmail(email) == null &&
+          Validation.validatePass(pass) == null;
+    }).listen((isValid) {
+      btnSink.add(isValid);
+    });
+  }
+
+  dispose() {
+    _emailSubject.close();
+    _passSubject.close();
+    _btnSubject.close();
+  }
+}
