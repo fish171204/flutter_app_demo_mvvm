a747ce43685951c0630db30ad84fc0b642d22faa
Nguyễn Đăng Khoa <145481912+fish171204@users.noreply.github.com>
2025-03-23 23:53:12 +0700

    feat(home): design UI for Home page

---
+++ lib/home/home_view.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Home Page"),
+      ),
+      body: const HomeBody(),
+    );
+  }
+}
+
+class HomeBody extends StatelessWidget {
+  const HomeBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+        child: Container(
+      child: const Text("Home Page"),
+    ));
+  }
+}
